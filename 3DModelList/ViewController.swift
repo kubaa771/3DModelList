810c1d5899da89fe70720a65fe14f69454f78a5e
Jakub Iwaszek <kubaa771@gmail.com>
2021-06-01 14:59:31 +0200

    Initial Commit

---
+++ 3DModelList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  3DModelList
+//
+//  Created by Jakub Iwaszek on 01/06/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
